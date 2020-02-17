f6e7d3857ff902c9cd6c9fc0d7cf7bdaf5147441
Bruno Galvao <brunopgalvao@gmail.com>
2020-02-17 17:56:29 -0500

    wip: sort

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+const state = {
+    products: []
+}
+
+function onComponentDidMount() {
+    const articles = document.querySelectorAll('article')
+    for (let i = 0; i < articles.length; i++) {
+        const product = articles[i].querySelector('.product-name')
+        // const newProducts = [...state.products, articles[i]]
+        // state.products = newProducts
+        console.log(articles[i])
+    }
+    state.products = articles
+    console.log(state.products)
+}
+
+onComponentDidMount()
+
 function search() {
     let input, filter, main, article, a, i, txtValue;
     input = document.getElementById("search");
@@ -14,3 +32,20 @@ function search() {
         }
     }
 }
+
+const sortDiv = document.querySelector('.sort')
+sortDiv.addEventListener('change', function () {
+    const selectedValue = sortDiv[sortDiv.options.selectedIndex].value
+    if (selectedValue == 'title-ascending') {
+
+    } else if (selectedValue == 'title-descending') {
+        // sort reverse
+    } else if (selectedValue == 'price-descending') {
+
+    } else {
+        // price reverse
+    }
+    const articles = document.querySelectorAll('article')
+    articles[0] = articles[3]
+    document.querySelector('.cards').append(articles[0])
+})
